27438d8ab9f4b5d401a805b18b078c74e3871ada
Bebe Peng <bebepeng@gmail.com>
2014-05-06 14:41:41 -0600

    songs index displays all of a songs information

--- spec/features/songs_spec.rb
+++ spec/features/songs_spec.rb
@@ -3,13 +3,19 @@ require 'spec_helper'
 feature 'Songs Page' do
   scenario 'users can add a song' do
     visit songs_path
-    click_on 'Add Song'
+    click_on 'Add a New Song'
 
     fill_in 'Title', :with => 'My Heart Will Go On'
     fill_in 'Artist', :with => 'Celine Dion'
-    fill_in 'Youtube Url', :with => 'https://www.youtube.com/watch?v=DNyKDI9pn0Q'
+    fill_in 'YouTube URL', :with => 'https://www.youtube.com/watch?v=DNyKDI9pn0Q'
     click_on 'Create Song'
 
     expect(page).to have_content 'My Heart Will Go On'
+    expect(page).to have_content 'Celine Dion'
+    expect(page).to have_youtube_link 'https://www.youtube.com/watch?v=DNyKDI9pn0Q'
+  end
+
+  def have_youtube_link(url)
+    have_css("a[href='#{url}']")
   end
 end
