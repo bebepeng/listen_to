83add51008af2631aa4f605f688dbf4268f6beff
Bebe Peng <bebepeng@gmail.com>
2014-05-09 15:12:01 -0600

    [#71054306] Username fields are case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
-  validates_uniqueness_of :username
+  validates_uniqueness_of :username, :case_sensitive => false
   validates :password, :length => {:minimum => 8}
   has_secure_password
+
+  def self.find_by_case_insensitive_username(username)
+    User.find_by('lower(username) = ?', username.downcase)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,11 +9,33 @@ describe User do
 
       expect(user.errors[:username].length).to eq 1
     end
+
+    it 'does not consider case when validating uniqueness of user name' do
+      create_valid_user
+      user = User.new(:username => 'bebe')
+      user.valid?
+
+      expect(user.errors[:username].length).to eq 1
+    end
+
     it 'requires at lest 8 characters in their password' do
-      user = User.new(:password => 'bebe')
+      user = User.new(:password => 'bebepen')
       user.valid?
 
       expect(user.errors[:password].length).to eq 1
+
+      user = User.new(:password => 'bebepeng')
+      user.valid?
+
+      expect(user.errors[:password].length).to eq 0
+    end
+  end
+
+  describe 'finder' do
+    it 'finds the user regarless of username case' do
+      create_valid_user
+
+      expect(User.find_by_case_insensitive_username('bebe').username).to eq 'Bebe'
     end
   end
 
